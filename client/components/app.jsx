bae6cbb0cf4a92cd1084e5e7a4089683a4035384
Joanna Pham <jopham90@gmail.com>
2020-11-04 10:29:33 -0800

    took out codes that weren't needed

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Header from './header';
+import ProductList from './product-list';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -18,8 +20,22 @@ export default class App extends React.Component {
   }
 
   render() {
-    return this.state.isLoading
-      ? <h1>Testing connections...</h1>
-      : <h1>{this.state.message}</h1>;
+
+    // let message = null;
+
+    // this.state.isLoading
+    //   ? message = <h1>Testing connections...</h1>
+    //   : message = <h1>{this.state.message}</h1>;
+
+    return (
+
+      <div>
+        <div>
+          <Header/>
+          <ProductList/>
+        </div>
+      </div>
+
+    );
   }
 }
---
+++ client/components/product-list.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ProductListItem from './product-list-item';
+
+export default class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+    this.getProducts = this.getProducts.bind(this);
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    fetch('/api/products')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ products: data });
+      })
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    const productsArray = this.state.products.map((product, index) =>
+      <ProductListItem
+        product={product}
+        key={index}/>
+    );
+    return (
+      <div className="main-container">
+        <div className="d-flex justify-content-center flex-wrap m-2">
+          {productsArray}
+        </div>
+      </div>
+    );
+  }
+}
